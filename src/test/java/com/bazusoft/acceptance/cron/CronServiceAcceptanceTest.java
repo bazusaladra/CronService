bf2165d889737251640757242033857254814f52
Blaze Saladra <blaze@eucalyptuslabs.com>
2023-10-24 21:34:28 +0200

    formatting fixes

--- src/test/java/com/bazusoft/acceptance/cron/CronServiceAcceptanceTest.java
+++ src/test/java/com/bazusoft/acceptance/cron/CronServiceAcceptanceTest.java
@@ -11,47 +11,48 @@ class CronServiceAcceptanceTest {
   @Test
   public void shouldPrintHelpIfNoArguments() throws Exception {
 
-    String systemOutput =
-        tapSystemErrAndOut(
-            () -> CronServiceApplication.main());
+    String systemOutput = tapSystemErrAndOut(() -> CronServiceApplication.main());
 
     assertEquals(
-    String.join(System.lineSeparator(),
+        String.join(
+            System.lineSeparator(),
             "Please pass a valid CRON expression as an argument, for example:",
-            "*/15 0 1,15 * 1-5 /usr/bin/find", ""),
-            systemOutput);
+            "*/15 0 1,15 * 1-5 /usr/bin/find",
+            ""),
+        systemOutput);
   }
 
   @Test
   public void shouldPrintTheArgumentIfNotValid() throws Exception {
     String input = "*/15";
 
-    String systemOutput =
-        tapSystemErrAndOut(
-            () -> CronServiceApplication.main(input));
+    String systemOutput = tapSystemErrAndOut(() -> CronServiceApplication.main(input));
 
     assertEquals(
-            String.join(System.lineSeparator(),
+        String.join(
+            System.lineSeparator(),
             "line 1:4 mismatched input '<EOF>' expecting ' '",
             "Failed to process given CRON argument:",
-            "*/15", ""), systemOutput);
+            "*/15",
+            ""),
+        systemOutput);
   }
 
   @Test
   public void shouldPrintCronInvocations() throws Exception {
     String input = "*/15 0 1,15 * 1-5 /usr/bin/find";
 
-    String systemOutput =
-        tapSystemErrAndOut(
-            () -> CronServiceApplication.main(input));
+    String systemOutput = tapSystemErrAndOut(() -> CronServiceApplication.main(input));
 
-    assertEquals("""
+    assertEquals(
+        """
     minute        0 15 30 45
     hour          0
     day of month  1 15
     month         1 2 3 4 5 6 7 8 9 10 11 12
     day of week   1 2 3 4 5
     command       /usr/bin/find
-    """, systemOutput);
+    """,
+        systemOutput);
   }
 }
--- src/test/java/com/bazusoft/cron/visitor/CronVisitorUnitTest.java
+++ src/test/java/com/bazusoft/cron/visitor/CronVisitorUnitTest.java
@@ -27,14 +27,15 @@ class CronVisitorUnitTest {
 
     String actualOutput = cronVisitor.print(input);
 
-    assertEquals("""
+    assertEquals(
+        """
     minute        0 15 30 45
     hour          0
     day of month  1 15
     month         1 2 3 4 5 6 7 8 9 10 11 12
     day of week   1 2 3 4 5
     command       /usr/bin/find
-    """, actualOutput);
-
+    """,
+        actualOutput);
   }
 }
